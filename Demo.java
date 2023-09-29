a7b600dddf8496c5be46c56d97e57840c31d7b7f
Judy Zhu <judy@MacBook-Air.local>
2023-09-29 16:39:57 -0400

    OkHttp

---
+++ Demo.java
@@ -0,0 +1,12 @@
+
+public class Demo {
+    OkHttpClient client = new OkHttpClient().newBuilder()
+            .build();
+    MediaType mediaType = MediaType.parse("text/plain");
+    RequestBody body = RequestBody.create(mediaType, "");
+    Request request = new Request.Builder()
+            .url("https://api.spoonacular.com/recipes/findByIngredients?ingredients=carrots,tomatoes&number=10&limitLicense=true&ranking=1&ignorePantry=false&apiKey=058b6a2aa4e24c0186999c82e5b1f05e")
+            .method("GET", body)
+            .build();
+    Response response = client.newCall(request).execute();
+}
