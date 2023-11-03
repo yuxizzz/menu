6dc3b80a6fababb4a762588e2eec9829e7c7373d
yuxi <yuxi.zhu@mail.utoronto.ca>
2023-11-03 11:02:37 -0400

    create_recipe use case

--- src/interface_adapter/clear_users/ClearPresenter.java
+++ src/interface_adapter/clear_users/ClearPresenter.java
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-package src.interface_adapter.clear_users;
+package interface_adapter.clear_users;
 import interface_adapter.ViewManagerModel;
 import use_case.clear_users.ClearOutputBoundary;
 import use_case.clear_users.ClearOutputData;
