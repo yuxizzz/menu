6bf2df721f0f5ca135a856c6749cc471f909b493
Hong Nie <hong.nie@mail.utoronto.ca>
2023-11-07 20:54:37 -0500

    interface_adapter of create folder

--- src/interface_adapter/create_folder/CreateFolderController.java
+++ src/interface_adapter/create_folder/CreateFolderController.java
@@ -1,2 +1,16 @@
-package interface_adapter.create_folder;public class CreateFolderController {
+package interface_adapter.create_folder;
+
+import use_case.create_folder.CreateFolderInputBoundary;
+import use_case.create_folder.CreateFolderInputData;
+
+public class CreateFolderController {
+    final CreateFolderInputBoundary createFolderUseCaseInteractor;
+
+    public CreateFolderController(CreateFolderInputBoundary createFolderUseCaseInteractor) {
+        this.createFolderUseCaseInteractor = createFolderUseCaseInteractor;
+    }
+    public void execute(String foldername) {
+        CreateFolderInputData createFolderInputData = new CreateFolderInputData(foldername);
+        createFolderUseCaseInteractor.execute(createFolderInputData);
+    }
 }
