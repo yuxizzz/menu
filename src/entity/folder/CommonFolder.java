ab0eafadefd75fe4eb82b2e91c35bfb571db5416
yuxi <yuxi.zhu@mail.utoronto.ca>
2023-11-09 18:40:13 -0500

    modified CommonFolder.java

--- src/entity/folder/CommonFolder.java
+++ src/entity/folder/CommonFolder.java
@@ -1,4 +1,24 @@
 package entity.folder;
 
-public class CommonFolder {
+import entity.recipe.Recipe;
+
+import java.util.HashMap;
+
+public class CommonFolder implements Folder {
+    private String folderName;
+    private HashMap<Integer, Recipe> recipeMap;
+
+    @Override
+    public void removeRecipe(Integer recipeID) {
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public HashMap getRecipeMap() {
+        return null;
+    }
 }
