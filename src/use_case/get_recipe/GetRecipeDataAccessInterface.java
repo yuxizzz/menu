a353f9c603c5ccdaedb0c4c89584763d604cc28d
Hong Nie <hong.nie@mail.utoronto.ca>
2023-11-10 21:07:19 -0500

    open recipe and get recipe

---
+++ src/use_case/get_recipe/GetRecipeDataAccessInterface.java
@@ -0,0 +1,7 @@
+package use_case.get_recipe;
+
+import entity.folder.Folder;
+
+public interface GetRecipeDataAccessInterface {
+    void save(Folder folder);
+}
---
+++ src/use_case/get_recipe/GetRecipeInputBoundary.java
@@ -0,0 +1,5 @@
+package use_case.get_recipe;
+
+public interface GetRecipeInputBoundary {
+    void execute(GetRecipeInputData getRecipeInputData);
+}
