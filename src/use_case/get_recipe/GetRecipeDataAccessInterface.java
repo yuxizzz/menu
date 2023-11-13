d2fcdf0a09bbf4181659ec2e8d46688e597af751
Hong Nie <hong.nie@mail.utoronto.ca>
2023-11-13 10:21:22 -0500

    get recipe

--- src/use_case/get_recipe/GetRecipeDataAccessInterface.java
+++ src/use_case/get_recipe/GetRecipeDataAccessInterface.java
@@ -3,5 +3,7 @@ package use_case.get_recipe;
 import entity.folder.Folder;
 
 public interface GetRecipeDataAccessInterface {
+
+//    boolean existsByName(Integer recipeID);
     void save(Folder folder);
 }
--- src/use_case/get_recipe/GetRecipeOutputBoundary.java
+++ src/use_case/get_recipe/GetRecipeOutputBoundary.java
@@ -1,4 +1,6 @@
 package use_case.get_recipe;
 
 public interface GetRecipeOutputBoundary {
+    void prepareSuccessView(GetRecipeOutputData recipe);
+    void prepareFailView(String error);
 }
