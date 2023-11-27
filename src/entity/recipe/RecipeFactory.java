46d930fa4f82e916cfcd3df9ae110483b19436a6
liantia2 <tiantian.lian@mail.utoronto.ca>
2023-11-27 00:05:51 -0500

    Add part of upload use case

--- src/entity/recipe/RecipeFactory.java
+++ src/entity/recipe/RecipeFactory.java
@@ -1,6 +1,8 @@
 package entity.recipe;
 
 public interface RecipeFactory {
-    Recipe create(String name, String ingredients, String nutrition, String instructions,
-                 String image, String recipeurl, Integer recipeID);
+    static Recipe create(String name, String ingredients, String nutrition, String instructions,
+                         String image, String recipeurl, Integer recipeID) {
+        return null;
+    }
 }
