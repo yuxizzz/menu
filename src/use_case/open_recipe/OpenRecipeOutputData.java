89e638094436ba267ce55c991d89b4ee067918f6
Hong Nie <hong.nie@mail.utoronto.ca>
2023-11-27 10:47:08 -0500

    use_case of open recipe

--- src/use_case/open_recipe/OpenRecipeOutputData.java
+++ src/use_case/open_recipe/OpenRecipeOutputData.java
@@ -1,4 +1,19 @@
 package use_case.open_recipe;
 
+import entity.recipe.Recipe;
+
+import java.io.IOException;
+
 public class OpenRecipeOutputData {
+    private Recipe recipe;
+    private boolean useCaseFailed;
+
+    public OpenRecipeOutputData(Recipe recipe, boolean useCaseFailed) throws IOException {
+//        this.recipeID = recipeID;
+        this.useCaseFailed = useCaseFailed;
+        this.recipe = recipe;
+    }
+    public Recipe getRecipe() {
+        return recipe;
+    }
 }
