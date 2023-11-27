da0e34b31809c952aecd737d4f46818f65f551e6
Your Name <shiqi.zuo@mail.utoronto.ca>
2023-11-27 10:57:11 -0500

    update state and add username

--- src/interface_adapter/open_recipe/OpenRecipeState.java
+++ src/interface_adapter/open_recipe/OpenRecipeState.java
@@ -1,4 +1,21 @@
 package interface_adapter.open_recipe;
 
+import interface_adapter.edit_recipe.EditRecipeState;
+
 public class OpenRecipeState {
+    private String username = "";
+
+    public OpenRecipeState(OpenRecipeState copy) {
+        username = copy.username;
+    }
+
+    // Because of the previous copy constructor, the default constructor must be explicit.
+    public OpenRecipeState() {}
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
