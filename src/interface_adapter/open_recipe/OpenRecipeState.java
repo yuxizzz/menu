5d585f1b9b670daf8284ffa28e8c88d9194c0100
Hong Nie <hong.nie@mail.utoronto.ca>
2023-12-01 20:53:06 -0500

    get/open recipe state

--- src/interface_adapter/open_recipe/OpenRecipeState.java
+++ src/interface_adapter/open_recipe/OpenRecipeState.java
@@ -13,20 +13,24 @@ public class OpenRecipeState {
     private String recipeIDError = null;
 
     private String username = "";
+    private String ingredients;
+    private String nutrition;
+    private String instructions;
+    private String image;
+    private String recipeurl;
 
 
     public OpenRecipeState(OpenRecipeState copy) {
-
         recipename = copy.recipename;
-
         recipenameError = copy.recipenameError;
-
         recipeID = copy.recipeID;
-
         recipeIDError = copy.recipeIDError;
-
         username = copy.username;
-
+        ingredients = copy.ingredients;
+        nutrition = copy.nutrition;
+        instructions = copy.instructions;
+        image = copy.image;
+        recipeurl = copy.recipeurl;
     }
 
 
@@ -72,6 +76,41 @@ public class OpenRecipeState {
     }
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    /**
+     * set-recipe and get-recipe
+     */
+    public void setRecipeurl(String recipeurl) {
+        this.recipeurl = recipeurl;
+    }
+    public void setImage(String image) {
+        this.image = image;
+    }
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+    public void setNutrition(String nutrition) {
+        this.nutrition = nutrition;
+    }
+
+    public String getRecipeurl() {
+        return recipeurl;
+    }
+    public String getImage() {
+        return image;
+    }
+    public String getIngredients() {
+        return ingredients;
+    }
+    public String getInstructions() {
+        return instructions;
+    }
+    public String getNutrition() {
+        return nutrition;
     }
 }
 
