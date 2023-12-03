05d1406476c067f6dec7fd5e5d26f65e9a2bab88
Your Name <shiqi.zuo@mail.utoronto.ca>
2023-12-03 00:54:00 -0500

    update use case factory

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,12 +3,22 @@ package app;
 import data_access.FileFolderDataAccessObject;
 import data_access.FileRecipeDataAccessObject;
 import data_access.FileUserDataAccessObject;
+import data_access.SearchResultsDataAccessObject;
 import entity.folder.CommonFolderFactory;
+import entity.folder.Folder;
 import entity.folder.FolderFactory;
 import entity.recipe.CommonRecipeFactory;
+import entity.search_results.CommonSearchResult;
+import entity.search_results.CommonSearchResultFactory;
+import entity.search_results.SearchResult;
 import entity.user.CommonUserFactory;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.clear_users.ClearViewModel;
+import interface_adapter.collect_recipe.CollectRecipeViewModel;
+import interface_adapter.create_folder.CreateFolderViewModel;
+import interface_adapter.default_opened_folder.DefaultOpenedFolderViewModel;
+import interface_adapter.delete_folder.DeleteFolderViewModel;
+import interface_adapter.delete_userRecipe.DeleteRecipeViewModel;
 import interface_adapter.get_recipe.GetRecipeViewModel;
 import interface_adapter.logged_in.LoggedInViewModel;
 import interface_adapter.login.LoginViewModel;
@@ -16,14 +26,23 @@ import interface_adapter.logout.LogoutController;
 import interface_adapter.logout.LogoutViewModel;
 import interface_adapter.my_folder.MyFolderController;
 import interface_adapter.my_folder.MyFolderViewModel;
+import interface_adapter.open_folder.OpenFolderViewModel;
+import interface_adapter.open_recipe.OpenRecipeViewModel;
 import interface_adapter.opened_folder.OpenedFolderViewModel;
+import interface_adapter.remove_recipe.RemoveViewModel;
 import interface_adapter.search.SearchController;
 import interface_adapter.search.SearchViewModel;
 import interface_adapter.searched.SearchedViewModel;
 import interface_adapter.signup.SignupViewModel;
+import interface_adapter.upload_recipe.UploadRecipeViewModel;
 import use_case.clear_users.ClearUserDataAccessInterface;
+import use_case.create_folder.CreateFolderDataAccessInterface;
+import use_case.delete_folder.DeleteFolderUserDataAccessInterface;
+import use_case.get_recipe.GetRecipeDataAccessInterface;
+import use_case.get_recipe.GetRecipeInputBoundary;
 import use_case.logout.LogoutDataAccessInterface;
 import use_case.my_folder.MyFolderDataAccessInterface;
+import use_case.open_folder.OpenFolderDataAccessInterface;
 import use_case.search.SearchUserDataAccessInterface;
 import view.*;
 
@@ -68,7 +87,7 @@ public class Main {
         FileRecipeDataAccessObject recipeDataAccessObject;
         try {
             recipeDataAccessObject = new FileRecipeDataAccessObject("./recipes.csv",
-                    new CommonRecipeFactory(), userDataAccessObject);
+                    new CommonRecipeFactory());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -81,12 +100,21 @@ public class Main {
             throw new RuntimeException(e);
         }
 
+        SearchResultsDataAccessObject searchResultsDataAccessObject;
+        try {
+            searchResultsDataAccessObject = new SearchResultsDataAccessObject("./search.csv",
+                    new CommonSearchResultFactory());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
         SignupView signupView = SignupUseCaseFactory.create(viewManagerModel, loginViewModel,
                 signupViewModel,userDataAccessObject);
         views.add(signupView, signupView.viewName);
 
         SearchViewModel searchViewModel = new SearchViewModel();
-        SearchUserDataAccessInterface searchUserDataAccessObject = (SearchUserDataAccessInterface) userDataAccessObject;
+        SearchUserDataAccessInterface searchUserDataAccessObject = searchResultsDataAccessObject;
         LoginView loginView = LoginUseCaseFactory.create(viewManagerModel, searchViewModel, loginViewModel, loggedInViewModel,
         userDataAccessObject);
         views.add(loginView, loginView.viewName);
@@ -95,17 +123,6 @@ public class Main {
         MyFolderViewModel myFolderViewModel = new MyFolderViewModel();
         OpenedFolderViewModel openedFolderViewModel = new OpenedFolderViewModel();
         LogoutViewModel logoutViewModel = new LogoutViewModel();
-
-
-
-        // TODO already created
-//        FileUserDataAccessObject userDataAccessObject;
-//        try {
-//            userDataAccessObject = new FileUserDataAccessObject("./users.csv", new CommonUserFactory());
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-
 
 
         GetRecipeViewModel getRecipeViewModel = new GetRecipeViewModel();
@@ -117,7 +134,62 @@ public class Main {
                 userDataAccessObject);
         views.add(loggedInView, loggedInView.viewName);
 
-//        MyFolderView myFolderView = MY
+        OpenFolderViewModel openFolderViewModel = new OpenFolderViewModel();
+        DeleteFolderViewModel deleteFolderViewModel = new DeleteFolderViewModel();
+        CreateFolderViewModel createFolderViewModel = new CreateFolderViewModel();
+
+        OpenFolderDataAccessInterface openFolderDataAccessObject = folderDataAccessObject;
+        DeleteFolderUserDataAccessInterface deleteFolderUserDataAccessObject = userDataAccessObject;
+        CreateFolderDataAccessInterface createFolderDataAccessObject = userDataAccessObject;
+
+        UploadRecipeViewModel uploadRecipeViewModel = new UploadRecipeViewModel();
+        DefaultOpenedFolderViewModel defaultOpenedFolderViewModel = new DefaultOpenedFolderViewModel("My Recipes");
+        OpenRecipeViewModel openRecipeViewModel = new OpenRecipeViewModel();
+        RemoveViewModel removeViewModel = new RemoveViewModel();
+
+        DeleteRecipeViewModel deleteRecipeViewModel = new DeleteRecipeViewModel();
+        MyFolderView myFolderView = MyFolderUseCaseFactory.create(viewManagerModel,
+                myFolderViewModel,
+                logoutViewModel,
+                openFolderViewModel,
+                deleteFolderViewModel,
+                createFolderViewModel,
+                openedFolderViewModel,
+                openFolderDataAccessObject,
+                deleteFolderUserDataAccessObject,
+                createFolderDataAccessObject,
+                uploadRecipeViewModel,
+                defaultOpenedFolderViewModel,
+                openRecipeViewModel,
+                removeViewModel,
+                deleteRecipeViewModel);
+
+        SearchView searchview = SearchUseCaseFactory.create(viewManagerModel,
+                searchViewModel,
+                getRecipeViewModel,
+                searchedViewModel,
+                searchUserDataAccessObject);
+
+
+        CollectRecipeViewModel collectRecipeViewModel = new CollectRecipeViewModel();
+        GetRecipeDataAccessInterface getRecipeDataAccessObject = recipeDataAccessObject;
+        SearchedView searchedView = SearchedUseCaseFactory.create(viewManagerModel,
+                getRecipeViewModel,
+                searchedViewModel,
+                getRecipeDataAccessObject,
+                collectRecipeViewModel);
+
+        FolderFactory folderFactory = new CommonFolderFactory();
+
+        CreateFolderView createFolderView = CreateFolderUseCaseFactory.create(viewManagerModel,
+                createFolderViewModel,
+                createFolderDataAccessObject,
+                folderFactory);
+
+
+
+
+
 
 
         viewManagerModel.setActiveView(signupView.viewName);
