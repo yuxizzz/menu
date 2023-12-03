a21b7ea3bbfde3dbb3a07511f49c007af6b7ee62
Hong Nie <hong.nie@mail.utoronto.ca>
2023-12-03 00:22:52 -0500

    1.2

--- src/app/CreateFolderUseCaseFactory.java
+++ src/app/CreateFolderUseCaseFactory.java
@@ -2,11 +2,11 @@ package app;
 
 import interface_adapter.ViewManagerModel;
 import interface_adapter.collect_recipe.CollectRecipeViewModel;
+import interface_adapter.create_folder.CreateFolderController;
 import interface_adapter.create_folder.CreateFolderViewModel;
 import interface_adapter.edit_recipe.EditRecipeViewModel;
 import interface_adapter.open_recipe.OpenRecipeController;
 import interface_adapter.open_recipe.OpenRecipeViewModel;
-import interface_adapter.opened_folder.OpenedFolderViewModel;
 import interface_adapter.remove_recipe.RemoveController;
 import interface_adapter.remove_recipe.RemoveViewModel;
 import use_case.open_recipe.OpenRecipeDataAccessInterface;
@@ -21,22 +21,11 @@ public class CreateFolderUseCaseFactory {
     private CreateFolderUseCaseFactory() {}
     public static CreateFolderView creat(
             ViewManagerModel viewManagerModel,
-            CreateFolderViewModel createFolderViewModel,
-            OpenRecipeViewModel openRecipeViewModel,
-            EditRecipeViewModel editRecipeViewModel,
-            CollectRecipeViewModel collectRecipeViewModel,
-            RemoveViewModel removeViewModel,
-            RemoveFolderDataAccessInterface removeDataAccessObject,
-            OpenRecipeDataAccessInterface openRecipeDataAccessObject) {
+            CreateFolderViewModel createFolderViewModel) {
         try {
-            RemoveController removeController = createRemoveUseCase(viewManagerModel,
-                    openedFolderViewModel, removeViewModel, removeDataAccessObject);
-            OpenRecipeController openRecipeController = createOpenRecipeUseCase(viewManagerModel,
-                    openRecipeViewModel,editRecipeViewModel, collectRecipeViewModel,openRecipeDataAccessObject);
+            CreateFolderController createFolderController =
 
-            return new OpenedFolderView(openedFolderViewModel, removeController,
-                    openRecipeViewModel, openRecipeController,
-                    removeViewModel);
+            return new CreateFolderView(createFolderViewModel, createFolderController);
 
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, "Could not open user data file.");
