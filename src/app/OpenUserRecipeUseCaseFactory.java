9a945ef983a368f142ccdeb7c70f5a2fda00e97a
Hong Nie <hong.nie@mail.utoronto.ca>
2023-12-02 23:45:07 -0500

    user DAO

--- src/app/OpenUserRecipeUseCaseFactory.java
+++ src/app/OpenUserRecipeUseCaseFactory.java
@@ -3,12 +3,19 @@ package app;
 import entity.recipe.CommonRecipeFactory;
 import entity.recipe.RecipeFactory;
 import interface_adapter.ViewManagerModel;
+import interface_adapter.add_recipe_to_folder.AddRecipeToFolderViewModel;
+import interface_adapter.collect_recipe.CollectRecipeController;
+import interface_adapter.collect_recipe.CollectRecipePresenter;
 import interface_adapter.collect_recipe.CollectRecipeViewModel;
 import interface_adapter.edit_recipe.EditRecipeViewModel;
 import interface_adapter.open_recipe.OpenRecipeController;
 import interface_adapter.open_recipe.OpenRecipePresenter;
 import interface_adapter.open_recipe.OpenRecipeViewModel;
 import interface_adapter.remove_recipe.RemoveController;
+import use_case.collect_recipe.CollectRecipeDataAccessInterface;
+import use_case.collect_recipe.CollectRecipeInputBoundary;
+import use_case.collect_recipe.CollectRecipeInteractor;
+import use_case.collect_recipe.CollectRecipeOutputBoundary;
 import use_case.open_recipe.OpenRecipeDataAccessInterface;
 import use_case.open_recipe.OpenRecipeInputBoundary;
 import use_case.open_recipe.OpenRecipeInteractor;
@@ -27,15 +34,17 @@ public class OpenUserRecipeUseCaseFactory {
             OpenRecipeViewModel openRecipeViewModel,
             EditRecipeViewModel editRecipeViewModel,
             CollectRecipeViewModel collectRecipeViewModel,
+            AddRecipeToFolderViewModel addRecipeToFolderViewModel,
+            CollectRecipeDataAccessInterface fileDataAccessObject,
             OpenRecipeDataAccessInterface openRecipeDataAccessObject) {
         try {
             OpenRecipeController openRecipeController = createOpenRecipeUseCase(viewManagerModel,
                     openRecipeViewModel,editRecipeViewModel, collectRecipeViewModel,openRecipeDataAccessObject);
-            RemoveController removeController = createRemoveUseCase(viewManagerModel,
-                    openedFolderViewModel, removeViewModel, removeDataAccessObject);
+            CollectRecipeController collectRecipeController = createCollectRecipeUseCase(viewManagerModel,
+                    collectRecipeViewModel, addRecipeToFolderViewModel, fileDataAccessObject);
 
-            return new OpenedFolderView(removeController, openRecipeViewModel,
-                    openRecipeController, removeViewModel);
+            return new UserRecipeView(openRecipeViewModel, openRecipeController,
+                    collectRecipeViewModel, collectRecipeController, removeViewModel);
 
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, "Could not open user data file.");
@@ -57,4 +66,18 @@ public class OpenUserRecipeUseCaseFactory {
                 openRecipeDataAccessObject, openRecipeOutputBoundary);
 
         return new OpenRecipeController(openRecipeController);
+    }
+
+    private static CollectRecipeController createCollectRecipeUseCase(ViewManagerModel viewManagerModel,
+                                                                      CollectRecipeViewModel collectRecipeViewModel,
+                                                                      AddRecipeToFolderViewModel addRecipeToFolderViewModel,
+                                                                      CollectRecipeDataAccessInterface fileDataAccessObject) {
+        CollectRecipeOutputBoundary collectRecipeOutputBoundary =
+                new CollectRecipePresenter(viewManagerModel, collectRecipeViewModel, addRecipeToFolderViewModel);
+
+        CollectRecipeInputBoundary collectRecipeInteractor = new CollectRecipeInteractor(
+                fileDataAccessObject, collectRecipeOutputBoundary);
+        return new CollectRecipeController(collectRecipeInteractor);
+    }
 }
+
--- src/data_access/FileUserDataAccessObject.java
+++ src/data_access/FileUserDataAccessObject.java
@@ -3,6 +3,7 @@ import entity.folder.Folder;
 import entity.recipe.Recipe;
 import entity.user.User;
 import entity.user.UserFactory;
+import use_case.create_folder.CreateFolderDataAccessInterface;
 import use_case.login.LoginUserDataAccessInterface;
 import use_case.logout.LogoutDataAccessInterface;
 import use_case.signup.SignupUserDataAccessInterface;
@@ -17,7 +18,7 @@ import java.util.Map;
 import java.util.ArrayList;
 
 public class FileUserDataAccessObject implements SignupUserDataAccessInterface, LoginUserDataAccessInterface , ClearUserDataAccessInterface,
-        LogoutDataAccessInterface {
+        LogoutDataAccessInterface, CreateFolderDataAccessInterface {
 
     private final File csvFile;
 
@@ -99,6 +100,14 @@ public class FileUserDataAccessObject implements SignupUserDataAccessInterface, 
     @Override
     public boolean existsByName(String identifier) {
         return accounts.containsKey(identifier);
+    }
+
+    @Override
+    public void save(Folder folder, String username) {
+        User user = accounts.get(username);
+        user.addFolder(folder);
+//        folders.put(folder.getName(), folder);
+//        saveToCSV();
     }
 
     @Override
