5ffaa33b3c7e2aef37b5a74af7518872ac3ebc71
Hong Nie <hong.nie@mail.utoronto.ca>
2023-12-02 19:39:56 -0500

    recipe view done

--- src/view/RecipeView.java
+++ src/view/RecipeView.java
@@ -75,11 +75,8 @@ public class RecipeView extends JPanel implements ActionListener, PropertyChange
 
         label.setBounds(150, 250, 150, 150);
         label.setVisible(true);
-// set information?
+// set name
         label.setText(recipeName);
-        label.setText(ingredients);
-        label.setText(nutrition);
-        label.setText(instructions);
         // set text of label label.setIcon(image);
         label.setHorizontalTextPosition(JLabel.CENTER);
         // set text LEFT,CENTER, RIGHT of imageicon
@@ -102,6 +99,47 @@ public class RecipeView extends JPanel implements ActionListener, PropertyChange
         label.setHorizontalAlignment(JLabel.CENTER);
         // set horizontal position of icon+text within label
         label.setBounds(100, 10, 250, 250);
+
+// set text
+
+        JLabel label1 = new JLabel(ingredients);
+        label1.setVisible(true);
+//        label1.setBackground(Color.red);
+        label1.setText(ingredients);
+        label1.setHorizontalTextPosition(JLabel.CENTER);
+        label1.setVerticalTextPosition(JLabel.TOP);
+        label1.setOpaque(true);
+        label1.setVerticalAlignment(JLabel.CENTER);
+        // set vertical position of icon+text within label
+        label1.setHorizontalAlignment(JLabel.CENTER);
+        // set horizontal position of icon+text within label
+        label1.setBounds(100, 510, 900, 250);
+
+        JLabel label2 = new JLabel(nutrition);
+        label2.setVisible(true);
+//        label2.setBackground(Color.cyan);
+        label2.setText(nutrition);
+        label2.setHorizontalTextPosition(JLabel.CENTER);
+        label2.setVerticalTextPosition(JLabel.TOP);
+        label2.setOpaque(true);
+        label2.setVerticalAlignment(JLabel.CENTER);
+        // set vertical position of icon+text within label
+        label2.setHorizontalAlignment(JLabel.CENTER);
+        // set horizontal position of icon+text within label
+        label2.setBounds(350, 10, 250, 250);
+
+        JLabel label3 = new JLabel(instructions);
+        label3.setVisible(true);
+//        label3.setBackground(Color.ORANGE);
+        label3.setText(instructions);
+        label3.setHorizontalTextPosition(JLabel.CENTER);
+        label3.setVerticalTextPosition(JLabel.TOP);
+        label3.setOpaque(true);
+        label3.setVerticalAlignment(JLabel.CENTER);
+        // set vertical position of icon+text within label
+        label3.setHorizontalAlignment(JLabel.CENTER);
+        // set horizontal position of icon+text within label
+        label3.setBounds(350, 260, 250, 250);
 
 
         collect.addActionListener(
