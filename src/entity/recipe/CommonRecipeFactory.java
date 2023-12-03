50f85ae2cba2f507fb942ce991c23a60a13a0fff
Your Name <shiqi.zuo@mail.utoronto.ca>
2023-12-03 01:32:15 -0500

    update use case factory

--- src/entity/recipe/CommonRecipeFactory.java
+++ src/entity/recipe/CommonRecipeFactory.java
@@ -10,6 +10,11 @@ public class CommonRecipeFactory implements RecipeFactory {
         return new CommonRecipe(name, ingredients, nutrition,
                 instructions, image, recipeurl, recipeID);
     }
+    public UserRecipe create(String name, String ingredients, String nutrition,
+                         String instructions, String image, String recipeurl, Integer recipeID, String username) {
+        return new UserRecipe(name, ingredients, nutrition,
+                instructions, image, recipeurl, recipeID, username);
+    }
 
 
 }
