bd186720fdcf22f7a839f8102cf541f3e47a5f0a
Hong Nie <hong.nie@mail.utoronto.ca>
2023-12-03 00:37:48 -0500

    CreateFolderUseCaseFactory

--- src/app/CreateFolderUseCaseFactory.java
+++ src/app/CreateFolderUseCaseFactory.java
@@ -2,23 +2,15 @@ package app;
 
 import entity.folder.FolderFactory;
 import interface_adapter.ViewManagerModel;
-import interface_adapter.collect_recipe.CollectRecipeViewModel;
 import interface_adapter.create_folder.CreateFolderController;
 import interface_adapter.create_folder.CreateFolderPresenter;
 import interface_adapter.create_folder.CreateFolderViewModel;
-import interface_adapter.edit_recipe.EditRecipeViewModel;
-import interface_adapter.open_recipe.OpenRecipeController;
-import interface_adapter.open_recipe.OpenRecipeViewModel;
-import interface_adapter.remove_recipe.RemoveController;
-import interface_adapter.remove_recipe.RemoveViewModel;
 import use_case.create_folder.*;
-import use_case.open_recipe.OpenRecipeDataAccessInterface;
-import use_case.remove_recipe.RemoveFolderDataAccessInterface;
 import view.CreateFolderView;
-import view.OpenedFolderView;
 
 import javax.swing.*;
 import java.io.IOException;
+
 
 public class CreateFolderUseCaseFactory {
     private CreateFolderUseCaseFactory() {}
@@ -60,5 +52,6 @@ public class CreateFolderUseCaseFactory {
               createFolderViewModel, viewManagerModel);
         CreateFolderInputBoundary createInteractor = new CreateFolderInteractor(createFolderDataAccessInterface,
                 createFolderOutputBoundary, folderFactory);
+        return new CreateFolderController(createInteractor);
     }
 }
